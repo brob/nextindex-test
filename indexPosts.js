cd68ef53875a60e3c1d3c500a8669563dfeb9bd3
Bryan Robinson <bryanrobinson@US-MBP-BR.local>
2021-08-27 10:08:21 -0400

    Adds url to images and adds overall url

--- indexPosts.js
+++ indexPosts.js
@@ -28,7 +28,6 @@ async function updateIndex() {
         // reads each file in the posts directory
         if (err) throw err;
         files.forEach(file => {
-            console.log(file)
             // reads each file in the posts directory
             fs.readFile('./_posts/' + file, 'utf8', (err, data) => {
                 // parses the front matter of each file
@@ -36,7 +35,9 @@ async function updateIndex() {
                 const normalizedPost = {
                     ...parsed.attributes,
                     objectID: file.split('.').slice(0, -1).join('.'),
-                    "body": parsed.body
+                    "body": parsed.body,
+                    "url": `${process.env.VERCEL_URL}/posts/${file.split('.').slice(0, -1).join('.')}`,
+                    "ogImage": `${process.env.VERCEL_URL}${parsed.attributes.ogImage}`,
                 }
 
                 // adds the post to the array of posts
@@ -45,12 +46,12 @@ async function updateIndex() {
 
                 // when all files have been read, index the posts
                 if (posts.length == files.length) {
-                    // indexPosts(posts)
-                    console.log(posts)
                     const batchArray = customBatch(posts)
                     index.clearObjects().then(() => {
+                        console.log('posts cleared')
+
                         client.multipleBatch(batchArray).then((res,err) => {
-                            console.log('success', res)
+                            console.log('Posts successfully indexed', res)
                         }).catch(err => {
                             console.log('error', err)
                         })
